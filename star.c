c56180ce295e1fa71d4932e473a05c5ebd2705ee
resetartend <gshk6570@tukorea.ac.kr>
2025-09-03 17:18:48 +0900

    Update star.c

--- star.c
+++ star.c
@@ -4,11 +4,11 @@ int main() {
     int i, j;
 
     for (i = 0; i < 3; i++) {
-        // 공백 출력
+        // space
         for (j = 0; j < 2 - i; j++) {
             printf(" ");
         }
-        // 별 출력
+        // asterisk
         for (j = 0; j < 2 * i + 1; j++) {
             printf("*");
         }
